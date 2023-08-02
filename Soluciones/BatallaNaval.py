2e6851e76d6d9b818edf8faa683b3a97940debab
Samu-Ro <luciarodriguezleonardi@gmail.com>
2023-08-02 09:51:50 -0300

    commit python

---
+++ Soluciones/BatallaNaval.py
@@ -0,0 +1,17 @@
+# @title Solución
+import numpy as np
+
+# Crear un tablero de juego 5x5
+def crearTablero():
+tablero = np.zeros((5, 5))
+return tablero
+
+# Colocar tres barcos en posiciones aleatorias
+
+    #x, y = np.random.randint(0, 5, size=2)
+
+
+# Función para verificar si hay un barco o agua en las coordenadas dadas
+
+
+# Prueba la función con algunas coordenadas
