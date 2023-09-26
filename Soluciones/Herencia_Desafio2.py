cf1a6ea39c3e82a7d83a657c0e2b893a819e04a3
Samu-Ro <luciarodriguezleonardi@gmail.com>
2023-09-26 12:14:56 -0300

    desafio 2 herencia

--- Soluciones/Herencia_Desafio2.py
+++ Soluciones/Herencia_Desafio2.py
@@ -1,27 +1,4 @@
-
-# Definimos la clase base Autor
-class Autor:
-    def __init__(self, nombre):
-        self.nombre = nombre
-# Creamos una subclase Escritor que hereda de Autor
-class Escritor(Autor):
-    def __init__(self, nombre, genero):
-        super().__init__(nombre)
-        self.genero = genero
-# Definimos una segunda clase base
-class Academico:
-    def __init__(self, universidad):
-        self.universidad = universidad
-
-# Instanciamos un objeto de la clase Escritor
-"""escritor = Escritor("Gabriel García Márquez", "Realismo Mágico")
-print(escritor.nombre, escritor.genero)
-"""
-# Creamos una clase que hereda de Escritor y Academico
-class EscritorAcademico(Escritor, Academico):
-    def __init__(self, nombre, genero, universidad):
-        Escritor.__init__(self, nombre, genero)
-        Academico.__init__(self, universidad)
+from Herencia_Desafio1 import Autor
 """
 Desafío 2: 
 Implementa una clase Poeta que herede de Autor y tenga un atributo para el tipo de poesía que escribe.
@@ -31,3 +8,9 @@ class Poeta(Autor):
         super().__init__(nombre)
         self.tipo_poesia = tipo_poesia
 
+def main():
+    poeta = Poeta("Gabriel García Márquez", "triste")
+    print(poeta.nombre, poeta.tipo_poesia)
+
+if __name__ == "__main__":
+    main()
